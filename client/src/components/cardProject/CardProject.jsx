a0a1e2e670dc900e5d5c988a82bc73984cda8cec
Falekin7 <falekinldg@gmail.com>
2024-06-12 19:09:34 -0300

    arreglo de navbar

--- client/src/components/cardProject/CardProject.jsx
+++ client/src/components/cardProject/CardProject.jsx
@@ -1,14 +1,14 @@
+import styles from './CardProject.module.css';
 
-const CardProject = (props) => {
-    const { name, image, description } = props
-    return (
-        <div>
-            <h4>{name}</h4>
-            <img src={image} alt="project-image" />
-            <p>{description}</p>
-        </div>
-    )
+const CardProject = props => {
+	const { name, image, description } = props;
+	return (
+		<div>
+			<h4>{name}</h4>
+			<img src={image} alt='project-image' />
+			<p>{description}</p>
+		</div>
+	);
 };
 
-export default CardProject
-
+export default CardProject;
--- client/src/components/projectsContainer/ProjectsContainer.jsx
+++ client/src/components/projectsContainer/ProjectsContainer.jsx
@@ -1,20 +1,26 @@
-import CardProject from "../cardProject/CardProject";
+import CardProject from '../cardProject/CardProject';
+import styles from './ProjectsContainer.module.css';
 
 const projects = [
-    { id: 1, name: "Project 1", image: "url", description: "asdasd asdasd" },
-    { id: 2, name: "Project 2", image: "url", description: "asdasd asdasd" },
-    { id: 3, name: "Project 3", image: "url", description: "asdasd asdasd" }
-]
+	{ id: 1, name: 'Project 1', image: 'url', description: 'asdasd asdasd' },
+	{ id: 2, name: 'Project 2', image: 'url', description: 'asdasd asdasd' },
+	{ id: 3, name: 'Project 3', image: 'url', description: 'asdasd asdasd' },
+];
 
 const ProjectsContainer = () => {
-    return (
-        <div>
-            <h3>I am projects container!</h3>
-            {projects.map(project => (
-                <CardProject key={project.id} name={project.name} image={project.url} description={project.description} />
-            ))}
-        </div>
-    )
+	return (
+		<div>
+			<h3>Projects</h3>
+			{projects.map(project => (
+				<CardProject
+					key={project.id}
+					name={project.name}
+					image={project.url}
+					description={project.description}
+				/>
+			))}
+		</div>
+	);
 };
 
-export default ProjectsContainer
+export default ProjectsContainer;
