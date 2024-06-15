3901af6190c9529257f199d82a45d0d39305fd17
Falekin7 <falekinldg@gmail.com>
2024-06-15 11:40:54 -0300

    project section modif

--- client/src/components/cardProject/CardProject.jsx
+++ client/src/components/cardProject/CardProject.jsx
@@ -1,10 +1,11 @@
-// import styles from './CardProject.module.css';
+import styles from './CardProject.module.css';
+import image from './zeldris.jpg';
 
-const CardProject = ({ name, image, description }) => {
+const CardProject = ({ name, description }) => {
 	return (
-		<div>
-			<h4>{name}</h4>
+		<div className={styles.cardProject}>
 			<img src={image} alt={`${name} image`} />
+			<h4>{name}</h4>
 			<p>{description}</p>
 		</div>
 	);
--- client/src/components/projects/Projects.jsx
+++ client/src/components/projects/Projects.jsx
@@ -1,25 +1,39 @@
-// import styles from './Project.module.css';
+import styles from './Projects.module.css';
 import CardProject from '../cardProject/CardProject';
 
 const projects = [
-	{ id: 1, name: 'Project 1', image: 'url', description: 'asdasdasd' },
-	{ id: 2, name: 'Project 2', image: 'url', description: 'asdasdasd' },
-	{ id: 3, name: 'Project 3', image: 'url', description: 'asdasdfsdf' },
+	{
+		id: 1,
+		name: 'Name app1',
+		image: 'url',
+		description: 'Fullstack application',
+	},
+	{
+		id: 2,
+		name: 'Name app2',
+		image: 'url',
+		description: 'Frontend application',
+	},
+	{
+		id: 3,
+		name: 'Name app3',
+		image: 'url',
+		description: 'Backend application',
+	},
 ];
 
 const Projects = () => {
 	return (
-		<section id='projects'>
-			<div>
-				{projects.map(project => (
-					<CardProject
-						key={project.id}
-						name={project.name}
-						image={project.image}
-						description={project.description}
-					/>
-				))}
-			</div>
+		<section id='projects' className={styles.projectsSection}>
+			<h2>Projects</h2>
+			{projects.map(project => (
+				<CardProject
+					key={project.id}
+					name={project.name}
+					image={project.image}
+					description={project.description}
+				/>
+			))}
 		</section>
 	);
 };
