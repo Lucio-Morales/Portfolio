3f3998928f1c3c7ec06b603c0be0b5b48d532e04
Falekin7 <falekinldg@gmail.com>
2024-07-28 12:12:43 -0300

    retomando portfolio

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,4 +1,5 @@
-import styles from './Home.module.css'; // Archivo de estilos CSS módulo
+import styles from './Home.module.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 	return (
@@ -10,15 +11,21 @@ const Home = () => {
 				Tell me, what can I do for you?
 			</p>
 			<div className={styles.options}>
-				<div className={styles.option}>
-					<span>1.</span> I want to see your projects!
-				</div>
-				<div className={styles.option}>
-					<span>2.</span> I want to get to know you!
-				</div>
-				<div className={styles.option}>
-					<span>3.</span> I want to schedule a meeting!
-				</div>
+				<Link to='/projects' style={{ textDecoration: 'none' }}>
+					<div className={styles.option}>
+						<span>1.</span> I want to see your projects!
+					</div>
+				</Link>
+				<Link to='/about' style={{ textDecoration: 'none' }}>
+					<div className={styles.option}>
+						<span>2.</span> I want to get to know you!
+					</div>
+				</Link>
+				<Link to='/contact' style={{ textDecoration: 'none' }}>
+					<div className={styles.option}>
+						<span>3.</span> I want to schedule a meeting!
+					</div>
+				</Link>
 			</div>
 		</section>
 	);
--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { FaHome } from 'react-icons/fa';
 import { RiContactsFill } from 'react-icons/ri';
 import { GrProjects } from 'react-icons/gr';
@@ -7,24 +7,41 @@ import { MdContactPhone } from 'react-icons/md';
 import styles from './Nav.module.css';
 
 const Nav = () => {
+	const location = useLocation();
 	return (
 		<>
 			<nav className={styles.nav}>
 				<ul className={styles.list}>
 					<Link to='/' className={styles.listLink}>
-						<FaHome className={styles.icon} />
+						<FaHome
+							className={`${styles.icon} ${
+								location.pathname === '/' ? styles.active : ''
+							}`}
+						/>
 					</Link>
 
 					<Link to='/about' className={styles.listLink}>
-						<RiContactsFill className={styles.icon} />
+						<RiContactsFill
+							className={`${styles.icon} ${
+								location.pathname === '/about' ? styles.active : ''
+							}`}
+						/>
 					</Link>
 
 					<Link to='/projects' className={styles.listLink}>
-						<GrProjects className={styles.icon} />
+						<GrProjects
+							className={`${styles.icon} ${
+								location.pathname === '/projects' ? styles.active : ''
+							}`}
+						/>
 					</Link>
 
 					<Link to='/contact' className={styles.listLink}>
-						<MdContactPhone className={styles.icon} />
+						<MdContactPhone
+							className={`${styles.icon} ${
+								location.pathname === '/contact' ? styles.active : ''
+							}`}
+						/>
 					</Link>
 				</ul>
 			</nav>
